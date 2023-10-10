f1594a07f85f0a6641f82a56a90fe92b17de8682
CA9527 <143056311+CA9527@users.noreply.github.com>
2023-10-10 16:47:03 +0800

    Update main.js

--- World Clock/main.js
+++ World Clock/main.js
@@ -1,19 +1,29 @@
-const allzone = document.getElementById('allzone'), currentTime = document.getElementById('currentTime');
-currentTime.innerText = new Date().toLocaleString('en-uk', {timeStyle:'full'})
+const allzone = document.getElementById('allzone');
+const currentTime = document.getElementById('currentTime');
+let intervalId = null; // Variable to store the interval ID
+
+currentTime.innerText = new Date().toLocaleString('en-uk', { timeStyle: 'full' });
+
 fetch('worldclock.json')
-.then(res=>res.json())
-.then(data=>{
-    data.map(e=>{
-        const option = document.createElement('option')
-        option.innerText = e.timezone
-        allzone.appendChild(option)
+  .then(res => res.json())
+  .then(data => {
+    data.forEach(e => {
+      const option = document.createElement('option');
+      option.innerText = e.timezone;
+      allzone.appendChild(option);
     });
-})
-.catch(err=>console.log(err))
+  })
+  .catch(err => console.log(err));
 
-allzone.oninput = () => setInterval(()=>time(),1000)
+allzone.oninput = () => {
+  clearInterval(intervalId); // Clear the previous interval
+  intervalId = setInterval(() => time(), 1000); // Set a new interval
+};
 
-function time(){
-    const ctime = new Date().toLocaleString('en-uk',{timezone: allzone.value, timeStyle:'full'})
-    currentTime.innerText = ctime
+function time() {
+  const ctime = new Date().toLocaleString('en-uk', {
+    timeZone: allzone.value,
+    timeStyle: 'full'
+  });
+  currentTime.innerText = ctime;
 }
