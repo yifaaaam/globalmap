ea5cece81bf98351f6f5a83bb26f344f9d03531a
CA9527 <143056311+CA9527@users.noreply.github.com>
2023-10-10 17:50:30 +0800

    rollback

--- World Clock/main.js
+++ World Clock/main.js
@@ -28,9 +28,7 @@ function time() {
 
 // Function to initialize and update the current time
 function updateTime() {
-  const ctime = new Date().toLocaleString('en-uk', { timeZone: allzone.value, timeStyle: 'full' });
+  const ctime = new Date().toLocaleString('en-uk', { timeStyle: 'full' });
   currentTime.innerText = ctime;
   intervalId = setInterval(() => time(), 1000); // Start updating immediately
 }
-
-updateTime();
